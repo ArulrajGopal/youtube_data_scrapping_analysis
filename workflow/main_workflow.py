6b8f68d5d3e004fb25acc5860942e13984838bb2
Arulraj Gopal <arulrajgopal@outlook.com>
2025-03-11 01:00:34 +0530

    get video function creation

--- workflow/main_workflow.py
+++ workflow/main_workflow.py
@@ -1,13 +1,17 @@
 from utility import *
 
-channel_details_lst = extract_channel_details(channel_id_dict)
-
+channel_details_lst, channel_plylst_dic = extract_channel_details(channel_id_dict)
 print("channels details extracted successfully!")
-
 for response in channel_details_lst:
     load_dyanmo_db("channel_raw","channel_id",response)
-
 print("channels details loaded into dynamoDB successfully!")
+
+
+
+video_id_list_output,response_list_output = get_video_header(channel_plylst_dic)
+
+for response in response_list_output:
+    load_dyanmo_db("video_header_raw","playlist_id",response)
 
 
 # df = fetch_dynamo_data_into_pd_dataframe("channel_raw")
--- workflow/utility.py
+++ workflow/utility.py
@@ -30,6 +30,7 @@ def fetch_dynamo_data_into_pd_dataframe(table_name):
 def extract_channel_details(channel_id_dict):
     current_date = int(datetime.now().strftime("%Y%m%d%H"))
     channel_details_lst = []
+    channel_plylst_dic = {}
 
     for channel in channel_id_dict.values():
         request =youtube.channels().list(part="snippet,contentDetails,statistics", id=channel)
@@ -41,7 +42,81 @@ def extract_channel_details(channel_id_dict):
 
 
         channel_details_lst.append(response)
+        channel_plylst_dic[channel] = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-    return channel_details_lst
+
+    return channel_details_lst, channel_plylst_dic
 
 
+
+def get_video_header(playlist_id):
+  video_id_list = []
+  response_list = []
+
+  request = youtube.playlistItems().list(part='contentDetails',playlistId = playlist_id)
+  response = request.execute()
+
+  for i in range(len(response['items'])):
+    video_id_list.append(response['items'][i]['contentDetails']['videoId'])
+    response_list.append(response)
+
+  next_page_token = response.get('nextPageToken')
+  more_pages = True
+
+  while more_pages:
+    if next_page_token is None:
+      more_pages = False
+    else:
+        request = youtube.playlistItems().list(part='contentDetails',playlistId = playlist_id,maxResults = 50, pageToken = next_page_token)
+        response = request.execute()
+
+        for i in range(len(response['items'])):
+          video_id_list.append(response['items'][i]['contentDetails']['videoId'])
+          response_list.append(response)
+
+        next_page_token = response.get('nextPageToken')
+
+  return video_id_list,response_list
+
+
+
+def get_video_details(video_id_lst):
+  #dislikecount not available in the API
+  all_video_stats = []
+
+  for i in range(0, len(video_id_lst), 50):
+    request = youtube.videos().list(part="snippet,contentDetails,statistics",id=','.join(video_id_lst[i:i+50]))
+    response = request.execute()
+    all_video_stats.append(response)
+
+  return all_video_stats
+
+
+
+
+def get_video_header(channel_plylst_dic):
+    current_date = int(datetime.now().strftime("%Y%m%d%H"))
+    response_list_output = []
+    video_id_list_output = []
+   
+    for playlist_id in channel_plylst_dic.values():
+        video_id_list,response_list = get_video_header(playlist_id)
+
+        for response in response_list:
+            current_time = str(datetime.now())
+            response["video_id"] = response['items'][response]['contentDetails']['videoId']
+            response["load_dt"] = current_date
+            response["updated_time"] = current_time
+            response_list_output.append(response)
+
+
+        for video in range(0, len(video_id_list), 50):
+            request = youtube.videos().list(part="snippet,contentDetails,statistics",id=','.join(video_id_list[video:video+50]))
+            response = request.execute()
+            current_time = str(datetime.now())
+            response["video_id"] =video
+            response["load_dt"] = current_date
+            response["updated_time"] = current_time
+            video_id_list_output.append(response)
+        
+    return video_id_list_output,response_list_output
