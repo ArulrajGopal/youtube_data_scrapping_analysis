f72b33c7c91dee1fcc8cbfa1b273131b8bad7605
Arulraj Gopal <arulrajgopal@outlook.com>
2025-03-10 23:13:49 +0530

    update

--- workflow/main_workflow.py
+++ workflow/main_workflow.py
@@ -3,7 +3,7 @@ from utility import *
 channel_details_lst = extract_channel_details(channel_id_dict)
 
 for response in channel_details_lst:
-    load_dyanmo_db("channel_raw","primary_key",response)
+    load_dyanmo_db("channel_raw","channel_id",response)
 
 
 # video_id_list = []
