63a8478bdc33cc3a93840d53bf057714007a5ee3
Arulraj Gopal <arulrajgopal@outlook.com>
2025-08-03 16:47:48 +0530

    update

--- src/config.py
+++ src/config.py
@@ -17,20 +17,20 @@ postgres_password = os.environ.get("postgres_password")
 channel_id_dict= {
 "OPTIONWITHAK":"UCslsTpdkrVnZSwxawhJN6Ag"
 ,"EQSIS":"UCKTWY-rVwUqCxrVmPOlJyjA"
-# ,"financeboosan":"UCmfl6VteCu880D8Txl4vEag"
-# ,"CapitalZone":"UCxoM_zP4Cr9LpIEn3TEZhvg"
-# ,"TheMadrasTrader":"UCqxH7wzv-sMrCnjP3lM3Nmw"
-# ,"SagaContraTrading":"UChaRiZ3h9JlLCeO2pdWkxMw"
-# ,"ddnifty":"UCYRB8kDbaW6a1Gufr_qwVTA"
-# ,"TamilStockMarket":"UC8HLa3_4B_31-UMSiLXrsCQ"
-# ,"HumbledVillageTrader":"UCoxNE6QEFUAdKuqvjT-kNwA"
-# ,"TamilShare":"UCaGwH4JooqsvBt-gJtB85Lg"
-# ,"TiruppurBullsShares":"UCqhL6vNCwYLC9_jePXOIvBg"
-# ,"PRSundar":"UCS2NdYUmv_PUyyKeDAo5zYA"
-# ,"Muthaleetukalam":"UCahsYnjbRheSL7uv6jWbJ4w"
-# ,"CauveryBusiness" : "UCa1FSXPOxb0x8lZTYCbQJ5g"
-# ,"MoneyPechu" : "UC7fQFl37yAOaPaoxQm-TqSA"
-# ,"TradeAchievers":"UCzk4zJEoZMnjvpoN0HlKjHQ"
+,"financeboosan":"UCmfl6VteCu880D8Txl4vEag"
+,"CapitalZone":"UCxoM_zP4Cr9LpIEn3TEZhvg"
+,"TheMadrasTrader":"UCqxH7wzv-sMrCnjP3lM3Nmw"
+,"SagaContraTrading":"UChaRiZ3h9JlLCeO2pdWkxMw"
+,"ddnifty":"UCYRB8kDbaW6a1Gufr_qwVTA"
+,"TamilStockMarket":"UC8HLa3_4B_31-UMSiLXrsCQ"
+,"HumbledVillageTrader":"UCoxNE6QEFUAdKuqvjT-kNwA"
+,"TamilShare":"UCaGwH4JooqsvBt-gJtB85Lg"
+,"TiruppurBullsShares":"UCqhL6vNCwYLC9_jePXOIvBg"
+,"PRSundar":"UCS2NdYUmv_PUyyKeDAo5zYA"
+,"Muthaleetukalam":"UCahsYnjbRheSL7uv6jWbJ4w"
+,"CauveryBusiness" : "UCa1FSXPOxb0x8lZTYCbQJ5g"
+,"MoneyPechu" : "UC7fQFl37yAOaPaoxQm-TqSA"
+,"TradeAchievers":"UCzk4zJEoZMnjvpoN0HlKjHQ"
 }
 
 
@@ -41,19 +41,15 @@ dynamodb = boto3.resource(
     aws_secret_access_key=aws_secret_access_key
 )
 
-#postgres_connection
-host_name = '127.0.0.1'
-port = '5432'
-database = 'postgres'
 
-postgresql = psycopg2.connect(
-            host=host_name,
-            dbname=database,
-            user=postgress_user,
-            password = postgres_password,
-            port=port
-)
 
-sqlalchemy_engine = create_engine(f'postgresql+psycopg2://{postgress_user}:{postgres_password}@{host_name}:{port}/{database}')
+# PostgreSQL connection details
+db_user = 'postgres'
+db_password = 'Arulraj_1234'
+db_host = 'localhost'       
+db_port = '5432'
+db_name = 'postgres'
+
+
 
 
