4b3579b5aa96d68598d2e39f830312292dcc2a9f
Arulraj Gopal <arulrajgopal@outlook.com>
2025-08-04 15:09:14 +0530

    update

--- src/load_video_details_raw.py
+++ src/load_video_details_raw.py
@@ -1,3 +1,11 @@
 from datetime import datetime
 from config import *
 from utility import *
+
+
+df = read_from_sql("video_header_stage")
+video_id_list = df['video_id'].tolist()
+
+video_id_list = ["g3qMaKz_PK4","6vrNQ1mxmHc","69bCP0XhAkk","XC22G-5TM24","2JzaTCVNaB4","ptEn_JBQ-sQ","UZzCpYIM5yA","qpuvNboIHUA","87QF7dyoVVQ"]
+
+
