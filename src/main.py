e60f968586314b531455dd51bf06a00868faab0f
Arulraj Gopal <arulrajgopal@outlook.com>
2025-08-07 11:08:07 +0530

    update

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import subprocess
 
 scripts = [
-    'src/load_channel_details_raw.py',
-    'src/load_channel_details_stage.py',
-    'src/load_video_header_raw.py',
+    # 'src/load_channel_details_raw.py',
+    # 'src/load_channel_details_stage.py',
+    # 'src/load_video_header_raw.py',
     'src/load_video_header_stage.py',
-    'src/load_video_details_raw.py'
+    'src/load_video_details_raw.py',
+    'src/load_video_details_stage.py'
+    
     ]
+
+
 
 for script in scripts:
     result = subprocess.run(['python', script], capture_output=True, text=True)
