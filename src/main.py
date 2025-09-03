ee16a40cb49eafb88216237f9b04a2454154bb28
Arulraj Gopal <arulrajgopal@outlook.com>
2025-09-03 15:05:59 +0530

    update

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ scripts = [
     # 'src/load_video_details_raw.py',
     # 'src/load_video_details_stage.py'
     'src/load_comment_details_raw.py',
-    # 'src/load_comment_details_stage.py'
+    'src/load_comment_details_stage.py'
     ]
 
 
