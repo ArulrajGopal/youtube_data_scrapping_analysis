5bf98f36a36b0ec5545e4e892b90804de636b258
Arulraj Gopal <arulrajgopal@outlook.com>
2025-09-04 18:58:55 +0530

    update

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ scripts = [
     # 'src/cleanup.py',
     # 'src/load_channel_details_raw.py',
     # 'src/load_channel_details_stage.py',
-    'src/load_video_header_raw.py',
+    # 'src/load_video_header_raw.py',
     # 'src/load_video_header_stage.py',
-    # 'src/load_video_details_raw.py',
+    'src/load_video_details_raw.py',
     # 'src/load_video_details_stage.py',
     # 'src/load_comment_details_raw.py',
     # 'src/load_comment_details_stage.py'
